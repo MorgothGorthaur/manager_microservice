8cc276eb2d0790fe9f42d0ccfcc0617b908a05b6
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-16 01:51:18 +0200

    created modules

---
+++ model/src/main/kotlin/executor/service/Step.kt
@@ -0,0 +1,16 @@
+package executor.service
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Reference
+import org.springframework.data.redis.core.RedisHash
+
+@RedisHash("steps")
+data class Step(
+    val action: String,
+    val value: String,
+    @Reference val report: StepReport?
+) {
+    @get:Id
+    var id: String? = null
+}
+
---
+++ model/src/main/kotlin/executor/service/StepReport.kt
@@ -0,0 +1,15 @@
+package executor.service
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import java.time.LocalTime
+
+@RedisHash("step_reports")
+data class StepReport(
+    val startTime: LocalTime,
+    val endTime: LocalTime,
+    val errorMessage: String?
+) {
+    @get:Id
+    var id: String? = null
+}
