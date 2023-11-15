ae57d2d9e3129ff3ca14b00b895d8e5fbbf8958b
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-16 00:07:56 +0200

    Initial commit

---
+++ src/main/kotlin/executor/service/manager/ManagerApplication.kt
@@ -0,0 +1,11 @@
+package executor.service.manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ManagerApplication>(*args)
+}
