03f5b5b8eaa19a320ed2398456c912c971c3c3e9
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-16 16:59:09 +0200

    refactored

--- model/src/main/kotlin/executor/service/model/Scenario.kt
+++ model/src/main/kotlin/executor/service/model/Scenario.kt
@@ -2,6 +2,7 @@ package executor.service.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
 import org.springframework.data.redis.core.index.Indexed
 
 @RedisHash("scenarios")
@@ -13,4 +14,7 @@ data class Scenario(
 ) {
     @get:Id
     var id: String? = null
+
+    @TimeToLive
+    val expiration: Long = 604800
 }
