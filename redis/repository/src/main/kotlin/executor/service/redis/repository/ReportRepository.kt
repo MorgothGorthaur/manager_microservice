6374d4c83f0f66bc87c55a1bcc577694ad69dfae
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-23 00:59:33 +0200

    created necessary methods

--- redis/repository/src/main/kotlin/executor/service/redis/repository/ReportRepository.kt
+++ redis/repository/src/main/kotlin/executor/service/redis/repository/ReportRepository.kt
@@ -2,5 +2,13 @@ package executor.service.redis.repository
 
 import com.redis.om.spring.repository.RedisDocumentRepository
 import executor.service.model.ScenarioReport
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.PagingAndSortingRepository
 
-interface ReportRepository : RedisDocumentRepository<ScenarioReport, String>
+interface ReportRepository : RedisDocumentRepository<ScenarioReport, String>, PagingAndSortingRepository<ScenarioReport, String> {
+    fun findPageByScenarioId(id: String, pageable: Pageable): Page<ScenarioReport>
+    fun findTop10ByScenarioId(id: String): List<ScenarioReport>
+    fun findPageByErrorMessageIsNotNull(pageable: Pageable): Page<ScenarioReport>
+    fun findPageByErrorMessageIsNull(pageable: Pageable): Page<ScenarioReport>
+}
--- redis/repository/src/main/kotlin/executor/service/redis/repository/ScenarioRepository.kt
+++ redis/repository/src/main/kotlin/executor/service/redis/repository/ScenarioRepository.kt
@@ -2,8 +2,11 @@ package executor.service.redis.repository
 
 import com.redis.om.spring.repository.RedisDocumentRepository
 import executor.service.model.Scenario
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.PagingAndSortingRepository
 
-interface ScenarioRepository : RedisDocumentRepository<Scenario, String> {
-    fun findByNameContaining(name: String): List<Scenario>
-    fun findByNameContainingAndSiteContaining(name: String, site: String): List<Scenario>
+interface ScenarioRepository : RedisDocumentRepository<Scenario, String>, PagingAndSortingRepository<Scenario, String> {
+    fun findPageByNameContaining(name: String, pageable: Pageable): Page<Scenario>
+    fun findPageBySiteContaining(site: String, pageable: Pageable): Page<Scenario>
 }
