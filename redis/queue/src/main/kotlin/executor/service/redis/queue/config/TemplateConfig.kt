1219f7b690fbf78ec9899037baba6a7f63fed3b0
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-11-25 16:07:27 +0200

    refactored

--- redis/queue/src/main/kotlin/executor/service/redis/queue/config/TemplateConfig.kt
+++ redis/queue/src/main/kotlin/executor/service/redis/queue/config/TemplateConfig.kt
@@ -1,7 +1,5 @@
 package executor.service.redis.queue.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
@@ -11,8 +9,4 @@ import org.springframework.data.redis.core.StringRedisTemplate
 class TemplateConfig(private val jedisConnectionFactory: JedisConnectionFactory) {
     @Bean
     fun redisTemplate() = StringRedisTemplate().apply { connectionFactory = jedisConnectionFactory }
-
-    @Bean("queue_mapper")
-    fun mapper() = ObjectMapper().apply { registerModule(JavaTimeModule()) }
-
 }
--- redis/queue/src/main/kotlin/executor/service/redis/queue/listener/repot/ReportQueueListenerImpl.kt
+++ redis/queue/src/main/kotlin/executor/service/redis/queue/listener/repot/ReportQueueListenerImpl.kt
@@ -1,20 +1,18 @@
 package executor.service.redis.queue.listener.repot
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import executor.service.model.ScenarioReport
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Component
-
 
 @Component
 class ReportQueueListenerImpl(
     private val template: StringRedisTemplate,
-    @Qualifier("queue_mapper")
     private val mapper: ObjectMapper
 ) : ReportQueueListener {
     private val key = "report.queue.key"
     override fun poll() = template.opsForList().rightPop(key)
-        ?.let{ mapper.readValue(it, ScenarioReport::class.java) }
+        ?.let{ mapper.readValue<ScenarioReport>(it) }
 
 }
