2218217e06937313fd2ec1b070d933938f0f4f42
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-01-01 16:55:54 +0200

    added searchable annotation

--- model/src/main/kotlin/executor/service/model/ScenarioReport.kt
+++ model/src/main/kotlin/executor/service/model/ScenarioReport.kt
@@ -2,13 +2,16 @@ package executor.service.model
 
 import com.redis.om.spring.annotations.Document
 import com.redis.om.spring.annotations.Indexed
+import com.redis.om.spring.annotations.Searchable
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.TimeToLive
 import java.time.LocalDateTime
 
 @Document
 data class ScenarioReport(
+    @Searchable
     val name: String,
+    @Searchable
     val site: String,
     val stepReports: List<StepReport>,
     @Indexed val scenarioId: String,
