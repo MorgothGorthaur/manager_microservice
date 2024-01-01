2a2aeb1b079a9c0ab9176d5c997acdf34497bf53
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-01-01 17:20:31 +0200

    removed PagingAndSortingRepository

--- dao/src/main/kotlin/executor/service/dao/repository/ReportRepository.kt
+++ dao/src/main/kotlin/executor/service/dao/repository/ReportRepository.kt
@@ -4,9 +4,8 @@ import com.redis.om.spring.repository.RedisDocumentRepository
 import executor.service.model.ScenarioReport
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.repository.PagingAndSortingRepository
 
-interface ReportRepository : RedisDocumentRepository<ScenarioReport, String>, PagingAndSortingRepository<ScenarioReport, String> {
+interface ReportRepository : RedisDocumentRepository<ScenarioReport, String> {
     fun findPageByScenarioId(id: String, pageable: Pageable): Page<ScenarioReport>
     fun searchByName(name: String, pageable: Pageable): Page<ScenarioReport>
     fun searchBySite(site: String, pageable: Pageable): Page<ScenarioReport>
--- dao/src/main/kotlin/executor/service/dao/repository/ScenarioRepository.kt
+++ dao/src/main/kotlin/executor/service/dao/repository/ScenarioRepository.kt
@@ -4,9 +4,8 @@ import com.redis.om.spring.repository.RedisDocumentRepository
 import executor.service.model.Scenario
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.repository.PagingAndSortingRepository
 
-interface ScenarioRepository : RedisDocumentRepository<Scenario, String>, PagingAndSortingRepository<Scenario, String> {
+interface ScenarioRepository : RedisDocumentRepository<Scenario, String> {
     fun searchByName(name: String, pageable: Pageable): Page<Scenario>
     fun searchBySite(site: String, pageable: Pageable): Page<Scenario>
 }
