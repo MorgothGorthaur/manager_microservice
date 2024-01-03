c9ec75bf9bb0fa3d1b4bf12d1fefa5f9a14619c5
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-01-03 15:00:30 +0200

    added pattern generation

--- processing/src/main/kotlin/executor/service/processing/report/ReportProcessingServiceImpl.kt
+++ processing/src/main/kotlin/executor/service/processing/report/ReportProcessingServiceImpl.kt
@@ -1,13 +1,22 @@
 package executor.service.processing.report
 
 import executor.service.dao.repository.ReportRepository
+import executor.service.processing.query.QueryProcessor
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
-class ReportProcessingServiceImpl(private val repo: ReportRepository) : ReportProcessingService {
+class ReportProcessingServiceImpl(
+    private val repo: ReportRepository,
+    private val processor: QueryProcessor
+) : ReportProcessingService {
     override fun findAll(request: Pageable) = repo.findAll(request)
-    override fun findByScenarioId(scenarioId: String, request: Pageable) = repo.findPageByScenarioId(scenarioId, request)
-    override fun findByName(name: String, request: Pageable) = repo.searchByName(name, request)
-    override fun findBySite(site: String, request: Pageable) = repo.searchBySite(site, request)
+    override fun findByScenarioId(scenarioId: String, request: Pageable) =
+        repo.findPageByScenarioId(scenarioId, request)
+
+    override fun findByName(name: String, request: Pageable) =
+        processor.createPattern(name).run { repo.searchByName(this, request) }
+
+    override fun findBySite(site: String, request: Pageable) =
+        processor.createPattern(site).run { repo.searchBySite(site, request) }
 }
