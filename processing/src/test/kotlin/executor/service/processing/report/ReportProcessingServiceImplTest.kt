51da7d59a9bbdd6b03b0599f7465a443addc2d2c
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-01-10 16:25:01 +0200

    rewrote test

--- processing/src/test/kotlin/executor/service/processing/report/ReportProcessingServiceImplTest.kt
+++ processing/src/test/kotlin/executor/service/processing/report/ReportProcessingServiceImplTest.kt
@@ -2,6 +2,7 @@ package executor.service.processing.report
 
 import executor.service.dao.repository.ReportRepository
 import executor.service.model.ScenarioReport
+import executor.service.processing.paging.Paginator
 import executor.service.processing.query.QueryProcessor
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
@@ -9,64 +10,65 @@ import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.kotlin.*
 import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import java.time.LocalDateTime
 
 private const val SCENARIO_ID = "some id"
-
 private const val SCENARIO_NAME = "some name"
-
 private const val SCENARIO_SITE = "some site"
-
 private const val ERROR = "some error"
-
+private const val PAGE_NUM = 0
+private const val PAGE_SIZE = 10
 internal class ReportProcessingServiceImplTest {
     private lateinit var repo: ReportRepository
     private lateinit var service: ReportProcessingService
-    private lateinit var request: PageRequest
     private lateinit var processor: QueryProcessor
+    private lateinit var paginator: Paginator
     @BeforeEach
     fun init() {
         repo = mock()
         processor = mock()
-        service = ReportProcessingServiceImpl(repo, processor)
-        request = mock()
+        paginator = mock()
+        service = ReportProcessingServiceImpl(repo, processor, paginator)
     }
     @Test
     fun testFindByScenarioId() {
         val expected = listOf(getReport(ERROR))
-        whenever(repo.findByScenarioId(anyString(), any())).thenReturn(PageImpl(expected))
-        val result = service.findByScenarioId(SCENARIO_ID, request).content
+        whenever(repo.findByScenarioId(anyString())).thenReturn(expected)
+        whenever(paginator.paginate(eq(expected), eq(PAGE_NUM), eq(PAGE_SIZE))).thenReturn(PageImpl(expected))
+        val result = service.findByScenarioId(SCENARIO_ID, PAGE_NUM, PAGE_SIZE).content
         assertEquals(expected, result)
-        verify(repo).findByScenarioId(anyString(), any())
+        verify(repo).findByScenarioId(anyString())
     }
     @Test
     fun testFindByName() {
         val expected = listOf(getReport())
-        whenever(repo.searchByName(anyString(), any())).thenReturn(PageImpl(expected))
+        whenever(repo.searchByName(anyString())).thenReturn(expected)
+        whenever(paginator.paginate(eq(expected), eq(PAGE_NUM), eq(PAGE_SIZE))).thenReturn(PageImpl(expected))
         whenever(processor.createPattern(eq(SCENARIO_NAME))).thenReturn(SCENARIO_NAME)
-        val result = service.findByName(SCENARIO_NAME, request).content
+        val result = service.findByName(SCENARIO_NAME, PAGE_NUM, PAGE_SIZE).content
         assertEquals(expected, result)
-        verify(repo).searchByName(anyString(), any())
+        verify(repo).searchByName(anyString())
     }
     @Test
     fun testFindBySite() {
         val expected = listOf(getReport())
-        whenever(repo.searchBySite(anyString(), any())).thenReturn(PageImpl(expected))
+        whenever(repo.searchBySite(anyString())).thenReturn(expected)
         whenever(processor.createPattern(eq(SCENARIO_SITE))).thenReturn(SCENARIO_SITE)
-        val result = service.findBySite(SCENARIO_SITE, request).content
+        whenever(paginator.paginate(eq(expected), eq(PAGE_NUM), eq(PAGE_SIZE))).thenReturn(PageImpl(expected))
+        val result = service.findBySite(SCENARIO_SITE, PAGE_NUM, PAGE_SIZE).content
         assertEquals(expected, result)
-        verify(repo).searchBySite(anyString(), any())
+        verify(repo).searchBySite(anyString())
     }
 
     @Test
     fun testFindAll() {
         val expected = listOf(getReport())
-        whenever(repo.findAll(any<Pageable>())).thenReturn(PageImpl(expected))
-        val result = service.findAll(request).content
+        whenever(repo.findAll()).thenReturn(expected)
+        whenever(paginator.paginate(eq(expected), eq(PAGE_NUM), eq(PAGE_SIZE))).thenReturn(PageImpl(expected))
+        val result = service.findAll(PAGE_NUM, PAGE_SIZE).content
         assertEquals(expected, result)
-        verify(repo).findAll(any<Pageable>())
+        verify(repo).findAll()
     }
 
     private fun getReport(errorMessage: String? = null) = ScenarioReport(
--- processing/src/test/kotlin/executor/service/processing/scenario/ScenarioProcessingServiceImplTest.kt
+++ processing/src/test/kotlin/executor/service/processing/scenario/ScenarioProcessingServiceImplTest.kt
@@ -1,35 +1,43 @@
 package executor.service.processing.scenario
 
+import executor.service.dao.repository.ReportRepository
 import executor.service.dao.repository.ScenarioRepository
 import executor.service.model.Scenario
+import executor.service.processing.paging.Paginator
 import executor.service.processing.query.QueryProcessor
-import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.ArgumentMatchers.anyString
-import org.mockito.kotlin.*
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.data.domain.PageImpl
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
 
 
 private const val SCENARIO_NAME = "some name"
-
 private const val SCENARIO_SITE = "some site"
+private const val PAGE_NUM = 0
+private const val PAGE_SIZE = 10
+
+private const val SCENARIO_ID = "someId"
 
 internal class ScenarioProcessingServiceImplTest {
 
-    private lateinit var repo: ScenarioRepository
+    private lateinit var scenarioRepository: ScenarioRepository
+    private lateinit var reportRepository: ReportRepository
     private lateinit var service: ScenarioProcessingService
-    private lateinit var request: PageRequest
     private lateinit var processor: QueryProcessor
+    private lateinit var paginator: Paginator
 
     @BeforeEach
     fun init() {
-        repo = mock()
+        scenarioRepository = mock()
+        reportRepository = mock()
         processor = mock()
-        service = ScenarioProcessingServiceImpl(repo, processor)
-        request = mock()
+        paginator = mock()
+        service = ScenarioProcessingServiceImpl(scenarioRepository, reportRepository, processor, paginator)
     }
 
 
@@ -37,50 +45,54 @@ internal class ScenarioProcessingServiceImplTest {
     fun testAdd() {
         val scenario = getScenario()
         service.add(scenario)
-        verify(repo).save(scenario)
+        verify(scenarioRepository).save(scenario)
     }
 
     @Test
     fun testUpdate() {
-        val scenario = getScenario().apply { id = "someId" }
+        val scenario = getScenario().apply { id = SCENARIO_ID }
         service.update(scenario)
-        verify(repo).update(scenario)
+        verify(scenarioRepository).update(scenario)
     }
 
     @Test
     fun testDelete() {
-        val scenario = getScenario().apply { id = "someId" }
+        val scenario = getScenario().apply { id = SCENARIO_ID }
         service.delete(scenario)
-        verify(repo).delete(scenario)
+        verify(scenarioRepository).delete(scenario)
+        verify(reportRepository).deleteByScenarioId(scenario.id!!)
     }
 
     @Test
     fun tetFindAll() {
-        val expected = PageImpl(listOf<Scenario>())
-        whenever(repo.findAll(any<Pageable>())).thenReturn(expected)
-        val result = service.findAll(request)
-        assertSame(expected, result)
-        verify(repo).findAll(any<Pageable>())
+        val expected =listOf<Scenario>()
+        whenever(scenarioRepository.findAll()).thenReturn(expected)
+        whenever(paginator.paginate(eq(expected), eq(PAGE_NUM), eq(PAGE_SIZE))).thenReturn(PageImpl(expected))
+        val result = service.findAll(PAGE_NUM, PAGE_SIZE).content
+        assertEquals(expected, result)
+        verify(scenarioRepository).findAll()
     }
 
     @Test
     fun testFindByName() {
-        val expected = PageImpl(listOf<Scenario>())
-        whenever(repo.searchByName(anyString(), any())).thenReturn(expected)
+        val expected = listOf<Scenario>()
+        whenever(scenarioRepository.searchByName(anyString())).thenReturn(expected)
+        whenever(paginator.paginate(eq(expected), eq(PAGE_NUM), eq(PAGE_SIZE))).thenReturn(PageImpl(expected))
         whenever(processor.createPattern(eq(SCENARIO_NAME))).thenReturn(SCENARIO_NAME)
-        val result = service.findByName(SCENARIO_NAME, request)
-        assertSame(expected, result)
-        verify(repo).searchByName(anyString(), any())
+        val result = service.findByName(SCENARIO_NAME, PAGE_NUM, PAGE_SIZE).content
+        assertEquals(expected, result)
+        verify(scenarioRepository).searchByName(anyString())
     }
 
     @Test
     fun testFindBySite() {
-        val expected = PageImpl(listOf<Scenario>())
-        whenever(repo.searchBySite(anyString(), any())).thenReturn(expected)
+        val expected = listOf<Scenario>()
+        whenever(scenarioRepository.searchBySite(anyString())).thenReturn(expected)
         whenever(processor.createPattern(eq(SCENARIO_SITE))).thenReturn(SCENARIO_SITE)
-        val result = service.findBySite(SCENARIO_SITE, request)
-        assertSame(expected, result)
-        verify(repo).searchBySite(anyString(), any())
+        whenever(paginator.paginate(eq(expected), eq(PAGE_NUM), eq(PAGE_SIZE))).thenReturn(PageImpl(expected))
+        val result = service.findBySite(SCENARIO_SITE, PAGE_NUM, PAGE_SIZE).content
+        assertEquals(expected, result)
+        verify(scenarioRepository).searchBySite(anyString())
     }
 
     private fun getScenario() = Scenario(
