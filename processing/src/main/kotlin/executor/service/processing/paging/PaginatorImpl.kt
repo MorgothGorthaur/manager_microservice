162811b441a97bdd835ff5986a63d4979b0072d1
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-01-10 16:33:44 +0200

    refactored

--- processing/src/main/kotlin/executor/service/processing/paging/PaginatorImpl.kt
+++ processing/src/main/kotlin/executor/service/processing/paging/PaginatorImpl.kt
@@ -1,11 +1,16 @@
 package executor.service.processing.paging
 
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Component
 
 @Component
 class PaginatorImpl : Paginator {
-    override fun <T> paginate(list: List<T>, pageNum: Int, pageSize: Int) =
-        PageImpl(list, PageRequest.of(pageNum, pageSize), list.size.toLong())
+    override fun <T> paginate(list: List<T>, pageNum: Int, pageSize: Int): Page<T> {
+        val first = pageNum * pageSize
+        val last = (pageNum + 1) * pageSize
+        val pageContent = list.subList(first, last)
+        return PageImpl(pageContent, PageRequest.of(pageNum, pageSize), list.size.toLong())
+    }
 }
