674c75d9d0e2f4e30dbdc6150b118b435577919c
MorgothGorthaur <victortarasov13032001@gmail.com>
2024-01-10 16:10:46 +0200

    added deleting reports with scenario

--- dao/src/main/kotlin/executor/service/dao/repository/ReportRepository.kt
+++ dao/src/main/kotlin/executor/service/dao/repository/ReportRepository.kt
@@ -5,6 +5,7 @@ import executor.service.model.ScenarioReport
 
 interface ReportRepository : RedisDocumentRepository<ScenarioReport, String> {
     fun findByScenarioId(id: String): List<ScenarioReport>
+    fun deleteByScenarioId(id: String)
     fun searchByName(name: String): List<ScenarioReport>
     fun searchBySite(site: String): List<ScenarioReport>
 
--- processing/src/main/kotlin/executor/service/processing/scenario/ScenarioProcessingServiceImpl.kt
+++ processing/src/main/kotlin/executor/service/processing/scenario/ScenarioProcessingServiceImpl.kt
@@ -1,5 +1,6 @@
 package executor.service.processing.scenario
 
+import executor.service.dao.repository.ReportRepository
 import executor.service.dao.repository.ScenarioRepository
 import executor.service.model.Scenario
 import executor.service.processing.paging.Paginator
@@ -9,36 +10,38 @@ import org.springframework.stereotype.Service
 
 @Service
 class ScenarioProcessingServiceImpl(
-    private val repo: ScenarioRepository,
+    private val scenarioRepository: ScenarioRepository,
+    private val reportRepository: ReportRepository,
     private val processor: QueryProcessor,
     private val paginator: Paginator
 ) : ScenarioProcessingService {
     override fun add(scenario: Scenario) {
-        repo.save(scenario)
+        scenarioRepository.save(scenario)
     }
 
     override fun update(scenario: Scenario) {
-        repo.update(scenario)
+        scenarioRepository.update(scenario)
     }
 
     override fun delete(scenario: Scenario) {
-        repo.delete(scenario)
+        scenarioRepository.delete(scenario)
+        reportRepository.deleteByScenarioId(scenario.id!!)
     }
 
     override fun findAll(pageNum: Int, pageSize: Int): Page<Scenario> {
-        val data = repo.findAll()
+        val data = scenarioRepository.findAll()
         return paginator.paginate(data, pageNum, pageSize)
     }
 
     override fun findByName(name: String, pageNum: Int, pageSize: Int): Page<Scenario> {
         val searchPattern = processor.createPattern(name)
-        val data = repo.searchByName(searchPattern)
+        val data = scenarioRepository.searchByName(searchPattern)
         return paginator.paginate(data, pageNum, pageSize)
     }
 
     override fun findBySite(site: String, pageNum: Int, pageSize: Int): Page<Scenario> {
         val searchPattern = processor.createPattern(site)
-        val data = repo.searchBySite(searchPattern)
+        val data = scenarioRepository.searchBySite(searchPattern)
         return paginator.paginate(data, pageNum, pageSize)
     }
 }
